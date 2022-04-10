4cc1b35c1d8cbc753421fe8372b4a088ab09bf80
Mark Huang <hhzeos@gmail.com>
2022-04-10 11:54:45 +0800

    Add simple model

---
+++ models/simple.py
@@ -0,0 +1,50 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.nn import init
+
+
+class SimpleModel(nn.Module):
+    def __init__(self, feat_dim: int, time_dim: int, mid_dim: int, out_dim: int):
+        super(SimpleModel, self).__init__()
+
+        self.lstm = nn.LSTM(
+            input_size=feat_dim,
+            hidden_size=mid_dim,
+            num_layers=2,
+            bidirectional=True,
+            batch_first=True,
+            dropout=0.01,
+        )
+        self.conv = nn.Conv1d(in_channels=mid_dim * 2, out_channels=10, kernel_size=1)
+        self.fc = nn.Linear(in_features=time_dim * 10, out_features=out_dim)
+
+    def forward(self, x: torch.Tensor):
+        """
+        Args:
+            x: [Tensor] (batch_size, feat_dim, time_dim)
+        return:
+            output representation [Tensor] (batch_size, out_dim)
+        """
+        B = x.size(0)
+
+        x = x.permute(0, 2, 1)  # (batch_size, time_dim, feat_dim)
+
+        lstm_out, _ = self.lstm(x)  # (B, T, C=mid_dim * 2)
+
+        feat = lstm_out.permute(0, 2, 1)  # (B, C=mid_dim * 2, T)
+
+        feat = self.conv(feat)  # (B, C, T)
+        feat = F.relu(feat)  # (B, C, T)
+        feat = feat.reshape(B, -1)  # (B, C*T)
+        out = self.fc(feat)  # (B, out_dim)
+
+        return out
+
+
+if __name__ == "__main__":
+    model = SimpleModel(feat_dim=40, time_dim=972, mid_dim=30, out_dim=1)
+    x = torch.Tensor(np.random.rand(8, 40, 972))
+    y = model(x)
+    print(y.shape)
---
+++ train.py
@@ -0,0 +1,134 @@
+"""This file is a simple example script training a CNN model on mel spectrograms."""
+import argparse
+import logging
+from copy import deepcopy
+from pathlib import Path
+from typing import Callable, List, Optional, Union
+
+import torch
+from torch.utils.data import ConcatDataset, DataLoader
+from torchaudio.functional import compute_deltas
+
+from DataLoader import lfcc, load_directory_split_train_test, mfcc
+from models.simple import SimpleModel
+from trainer import GDTrainer
+from utils import set_seed_all
+
+LOGGER = logging.getLogger()
+
+
+def init_logger(log_file):
+    LOGGER.setLevel(logging.INFO)
+
+    # create file handler
+    fh = logging.FileHandler(log_file)
+
+    # create console handler
+    ch = logging.StreamHandler()
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    LOGGER.addHandler(fh)
+    LOGGER.addHandler(ch)
+
+
+def save_model(
+    model: torch.nn.Module,
+    model_dir: Union[Path, str],
+    name: str,
+    em: bool = False,
+    raw_net: bool = False,
+) -> None:
+    if raw_net:
+        model_class = "raw_net"
+    else:
+        model_class = "em" if em else "gd"
+    full_model_dir = Path(f"{model_dir}/{model_class}/{name}")
+    if not full_model_dir.exists():
+        full_model_dir.mkdir(parents=True)
+
+    torch.save(model.state_dict(), f"{full_model_dir}/ckpt.pth")
+
+
+def train(
+    real_dir: Union[Path, str],
+    fake_dir: List[Union[Path, str]],
+    amount_to_use: int = None,
+    epochs: int = 20,
+    device: str = "cuda" if torch.cuda.is_available else "cpu",
+    batch_size: int = 8,
+    model_dir: Optional[str] = None,
+    test_size: float = 0.2,
+) -> None:
+
+    LOGGER.info("Loading data...")
+
+    real_dataset_train, real_dataset_test = load_directory_split_train_test(
+        path=real_dir,
+        feature_fn=lfcc,
+        feature_kwargs={},
+        test_size=test_size,
+        use_double_delta=True,
+        phone_call=False,
+        pad=True,
+        label=1,
+        amount_to_use=amount_to_use,
+    )
+
+    fake_dataset_train, fake_dataset_test = load_directory_split_train_test(
+        path=fake_dir,
+        feature_fn=lfcc,
+        feature_kwargs={},
+        test_size=test_size,
+        use_double_delta=True,
+        phone_call=False,
+        pad=True,
+        label=0,
+        amount_to_use=amount_to_use,
+    )
+
+    dataset_train = ConcatDataset([real_dataset_train, fake_dataset_train])
+    LOGGER.info(f"Training model on {len(dataset_train)} audio files.")
+
+    model = SimpleModel(feat_dim=40, time_dim=972, mid_dim=30, out_dim=1)
+
+    model = GDTrainer(
+        device=device,
+        batch_size=batch_size,
+        epochs=epochs,
+        optimizer_kwargs={
+            "lr": 0.0001,
+            "weight_decay": 0.0001,
+        },
+    ).train(
+        dataset=dataset_train,
+        model=model,
+        test_len=test_size,
+    )
+
+    if model_dir is not None:
+        ...
+        save_model(
+            model,
+            model_dir,
+            str(fake_dir).strip("/").replace("/", "_"),
+            raw_net=True,
+        )
+
+
+def main():
+    # fix all seeds
+    set_seed_all(42)
+    init_logger("experiment.log")
+    LOGGER.setLevel(logging.DEBUG)
+    train(
+        real_dir="/home/markhh/Documents/DeepFakeAudioDetection/LJ_Speech",
+        fake_dir="/home/markhh/Documents/DeepFakeAudioDetection/WaveFake_generated_audio/ljspeech_waveglow",
+    )
+
+
+if __name__ == "__main__":
+    main()
