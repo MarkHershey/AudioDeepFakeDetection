04ac507d1eee1fbed9635351780f2dd05da22052
James R T <jamestiotio@gmail.com>
2022-04-11 02:32:04 +0800

    Add current ShallowCNN model code

---
+++ cnn/README.md
@@ -0,0 +1,10 @@
+# CNN Speech Classification Implementation
+
+This folder contains a custom implementation of the architecture described in this paper: ["Hello? Who Am I Talking to?" A Shallow CNN Approach for Human vs. Bot Speech Classification](https://ieeexplore.ieee.org/document/8682743).
+
+The datasets used are the same datasets used for the rest of this project, which consist of:
+
+- Human Voice Dataset: [LJ Speech](https://keithito.com/LJ-Speech-Dataset/)
+- Synthetic Voice Dataset: [WaveFake](https://zenodo.org/record/5642694)
+
+If you have any concerns or queries, feel free to direct them to [James Raphael Tiovalen](https://github.com/jamestiotio).
---
+++ cnn/models.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch import flatten
+
+
+class ShallowCNN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(ShallowCNN, self).__init__()
+        self.conv1 = nn.Conv2d(input_dim, 32, kernel_size=4, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 48, kernel_size=5, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(48, 64, kernel_size=4, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=(2, 4), stride=1, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(15104, 128)
+        self.fc2 = nn.Linear(128, output_dim)
+
+    def forward(self, x: torch.Tensor):
+        x = x.unsqueeze(1)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
