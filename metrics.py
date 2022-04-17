d6c3c1f27bb04ea626b7745bf02712870c19b1af
Mark Huang <hhzeos@gmail.com>
2022-04-18 01:42:14 +0800

    Update metrics

--- metrics.py
+++ metrics.py
@@ -1,9 +1,16 @@
+import json
+import os
+from pathlib import Path
+from typing import Tuple, Union
+
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.metrics as skm
+from scipy.interpolate import interp1d
+from scipy.optimize import brentq
 
 
-def compute_roc_auc_eer(y_true, y_pred):
+def compute_roc_auc_eer(y_true, y_pred) -> Tuple[float, float]:
     # Compute the ROC curve and the AUC
     fpr, tpr, thresholds = skm.roc_curve(y_true, y_pred, pos_label=1)
     roc_auc = skm.auc(fpr, tpr)
@@ -14,11 +21,8 @@ def compute_roc_auc_eer(y_true, y_pred):
     return roc_auc, eer
 
 
-def alt_compute_eer(y_true, y_pred):
+def alt_compute_eer(y_true, y_pred) -> float:
     # Ref: https://github.com/scikit-learn/scikit-learn/issues/15247#issuecomment-542138349
-    from scipy.interpolate import interp1d
-    from scipy.optimize import brentq
-
     fpr, tpr, thresholds = skm.roc_curve(y_true, y_pred, pos_label=1)
     eer = brentq(lambda x: 1.0 - x - interp1d(fpr, tpr)(x), 0.0, 1.0)
     return eer
@@ -52,5 +56,63 @@ def main():
     plt.show()
 
 
+def compute_metrics_for_file(
+    filename: Union[str, Path]
+) -> Tuple[float, float, float, float, float]:
+    filepath: Path = Path(filename)
+    if not filepath.exists():
+        raise FileNotFoundError(f"File {filepath} does not exist.")
+    with filepath.open("r") as f:
+        data = json.load(f)
+
+    acc: float = skm.accuracy_score(data["y_true"], data["y_pred"])
+    f1: float = skm.f1_score(data["y_true"], data["y_pred"])
+    eer: float = alt_compute_eer(data["y_true"], data["y_pred"])
+    roc_auc, eer2 = compute_roc_auc_eer(data["y_true"], data["y_pred"])
+
+    return acc, f1, roc_auc, eer, eer2
+
+
+def compute_all():
+    save_dir = Path(__file__).parent / "saved"
+    export_filepath = save_dir / "RESULT.md"
+    result = {}
+
+    for exp_name in os.listdir(save_dir):
+        pred_filepath = save_dir / exp_name / "best_pred.json"
+        if not pred_filepath.is_file():
+            continue
+
+        acc, f1, roc_auc, eer, eer2 = compute_metrics_for_file(pred_filepath)
+        result[exp_name] = dict(
+            acc=f"{acc:.3f}",
+            f1=f"{f1:.3f}",
+            roc_auc=f"{roc_auc:.4f}",
+            eer=f"{eer:.4f}",
+            eer2=f"{eer2:.4f}",
+        )
+
+    to_write = [
+        "# Empirical Results",
+        " ",
+        "- Accuracy",
+        "- F1 score",
+        "- Area Under the Receiver Operating Characteristic Curve (ROC AUC)",
+        "- Equal Error Rate (EER)",
+        " ",
+        "| Experiment | Accuracy | F1 | ROC AUC | EER | EER2 |",
+        "| :--- | ---: | ---: | ---: | ---: | ---: |",
+    ]
+
+    for exp_name in sorted(result.keys(), key=lambda x: result[x]["f1"]):
+        d = result[exp_name]
+        to_write.append(
+            f"| {exp_name} | {d['acc']} | {d['f1']} | {d['roc_auc']} | {d['eer']} | {d['eer2']} |"
+        )
+
+    with export_filepath.open("w") as f:
+        f.write("\n".join(to_write))
+
+
 if __name__ == "__main__":
-    main()
+    compute_all()
---
+++ saved/RESULT.md
@@ -0,0 +1,19 @@
+# Empirical Results
+ 
+- Accuracy
+- F1 score
+- Area Under the Receiver Operating Characteristic Curve (ROC AUC)
+- Equal Error Rate (EER)
+ 
+| Experiment | Accuracy | F1 | ROC AUC | EER | EER2 |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| MLP_mfcc_I | 0.500 | 0.000 | 0.5000 | 0.5000 | 0.0000 |
+| WaveLSTM_wave_I | 0.500 | 0.667 | 0.5000 | 0.5000 | 0.0000 |
+| TSSD_wave_O | 0.899 | 0.897 | 0.8989 | 0.1164 | 0.0813 |
+| ShallowCNN_lfcc_O | 0.930 | 0.927 | 0.9296 | 0.0970 | 0.0374 |
+| SimpleLSTM_mfcc_I | 0.960 | 0.960 | 0.9601 | 0.0404 | 0.0405 |
+| SimpleLSTM_lfcc_O | 0.965 | 0.965 | 0.9651 | 0.0441 | 0.0248 |
+| SimpleLSTM_lfcc_I | 0.996 | 0.996 | 0.9962 | 0.0042 | 0.0034 |
+| ShallowCNN_mfcc_I | 0.997 | 0.997 | 0.9968 | 0.0049 | 0.0015 |
+| TSSD_wave_I | 0.999 | 0.999 | 0.9990 | 0.0011 | 0.0011 |
+| ShallowCNN_lfcc_I | 1.000 | 1.000 | 0.9996 | 0.0004 | 0.0004 |
